540fd723a1bbfb38447a578728012f409d79ec38
Bianca Souza <bianca.souza@aluno.faculdadeimpacta.com.br>
2021-09-02 17:53:13 -0300

    Criacao de Conexao DB teste

---
+++ Comanda-Eletronica/Data/ComandaEletronicaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Comanda_Eletronica.Data
+{
+    public class ComandaEletronicaContext : DbContext
+    {
+        public ComandaEletronicaContext(
+            DbContextOptions<ComandaEletronicaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Comanda_Eletronica.Models.Product> Products { get; set; }
+    }
+}
---
+++ Comanda-Eletronica/Startup.cs
@@ -0,0 +1,60 @@
+using Comanda_Eletronica.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Comanda_Eletronica
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+            configuration = Configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddRazorPages();
+
+            services.AddDbContext<ComandaEletronicaContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+}
