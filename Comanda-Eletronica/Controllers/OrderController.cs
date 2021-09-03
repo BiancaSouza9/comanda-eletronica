6f389b80cb2ae89de19c46bcb7dce69d1de6dfd5
Bianca Souza <bianca.souza@aluno.faculdadeimpacta.com.br>
2021-09-03 00:15:51 -0300

    Adicionando Controller e Ajustando Model exemplo

---
+++ Comanda-Eletronica/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Comanda_Eletronica.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace Comanda_Eletronica.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PedidoController : ControllerBase
+    {
+        private ComandaEletronicaContext Context;
+
+        private readonly ILogger<PedidoController> _logger;
+
+        public PedidoController(ILogger<PedidoController> logger, ComandaEletronicaContext _context)
+        {
+            _logger = logger;
+            Context = _context;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(Context.Orders.Any());
+        }
+    }
+}
---
+++ Comanda-Eletronica/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Comanda_Eletronica.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+    }
+}
